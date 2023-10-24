c5092f0fb19c04b44843c2e5966ed7a6093b5f2f
PCKANU\Karel <gsus>
2023-10-24 16:52:37 -0700

    - Se modifica texto para prueba

--- Punto de Venta/Vistas/View_Cuenta.Designer.cs
+++ Punto de Venta/Vistas/View_Cuenta.Designer.cs
@@ -173,7 +173,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(270, 40);
             this.button2.TabIndex = 13;
-            this.button2.Text = "INICIAR";
+            this.button2.Text = "INICIAR SESION";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
